9a5543df8f7534cfd30b59db77d5996ddaa646b4
Tore Bjolseth <tbjolset@cisco.com>
2022-06-21 22:12:00 +0200

    add example to test usermedia (camera and microphone access)

---
+++ usermedia/index.js
@@ -0,0 +1,56 @@
+async function initVideo() {
+  const props = {
+    video: true,
+    audio: false,
+  };
+
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(props);
+    document.querySelector('#camera-feed').srcObject = stream;
+  }
+  catch(e) {
+    console.warn('no video');
+    document.querySelector('#camera').innerHTML = 'Camera not available';
+  }
+}
+
+function sampleMicLevel(stream) {
+  const audioContext = new AudioContext();
+  const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream);
+  const analyserNode = audioContext.createAnalyser();
+  mediaStreamAudioSourceNode.connect(analyserNode);
+
+  const pcmData = new Float32Array(analyserNode.fftSize);
+  const analyse = () => {
+      analyserNode.getFloatTimeDomainData(pcmData);
+      let sumSquares = 0.0;
+      for (const amplitude of pcmData) {
+        sumSquares += amplitude*amplitude;
+      }
+      const level =  Math.sqrt(sumSquares / pcmData.length);
+      document.querySelector('#audio-level').innerHTML = parseInt(level * 100);
+      // volumeMeterEl.value = Math.sqrt(sumSquares / pcmData.length);
+  };
+  setInterval(analyse, 200);
+}
+
+async function initAudio() {
+  const props = { audio: true, video: false };
+  try {
+    const audio = await navigator.mediaDevices.getUserMedia(props);
+    console.log('got audio');
+    sampleMicLevel(audio);
+  }
+  catch(e) {
+    console.warn('no audio');
+    document.querySelector('#audio').innerHTML = 'Microphone not available';
+
+  }
+}
+
+function init() {
+  initVideo();
+  initAudio();
+}
+
+window.onload = init;
