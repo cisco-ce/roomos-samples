ef8f92d5ca039410a0443ae17af8c0317f370a43
Tore Bjolseth <tbjolset@cisco.com>
2022-10-07 14:16:19 +0200

    visitor kiosk: finish main flows

--- visitor-kiosk/index.js
+++ visitor-kiosk/index.js
@@ -1,8 +1,44 @@
 
 const dataModel = {
-  page: 'home',
+  page: 'home', // 'home', 'checkIn', 'findHost', 'checkOut', 'registered'
+  name: '',
+  email: '',
+  host: '',
 
   init() {
+  },
+
+  home() {
+    this.page = 'home';
+  },
+
+  get validForm() {
+    // skip while developing
+    // if (this.page === 'checkIn') {
+    //   return this.name.trim().length && this.email.match(/\w+@\w+/);
+    // }
+    return true;
+  },
+
+  checkIn() {
+    this.page = 'checkIn';
+  },
+
+  findHost() {
+    this.page = 'findHost';
+    console.log('register', this.name, this.email);
+  },
+
+  registered() {
+    this.page = 'registered';
+  },
+
+  searchHost() {
+    console.log('search for', this.host);
+  },
+
+  checkOut() {
+    this.page = 'checkOut';
   },
 
 };
